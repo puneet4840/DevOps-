190cb4da1316125004c95babb6a1226bb3c046b8
Puneet Kumar <65063977+puneet4840@users.noreply.github.com>
2025-03-04 11:15:41 +0530

    Add files via upload

---
+++ 6 - Terraform/Terraform Code/4 - File and Directory Structure in Terraform/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  tenant_id = var.tenant_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  features {}
+}
---
+++ 6 - Terraform/Terraform Code/4 - File and Directory Structure in Terraform/variables.tf
@@ -0,0 +1,26 @@
+variable "subscription_id" {
+  type        = string
+}
+
+variable "tenant_id" {
+  type        = string
+}
+
+variable "client_id" {
+  type        = string
+}
+
+variable "client_secret" {
+  type        = string
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = ""
+}
+
+variable "resource_group_location" {
+  type        = string
+  default     = ""
+}
+
