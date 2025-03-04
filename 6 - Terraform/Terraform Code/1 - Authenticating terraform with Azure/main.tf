6347cf93c5279d2e78e140cdc81cba4e6420f484
Puneet Kumar <65063977+puneet4840@users.noreply.github.com>
2025-03-04 11:13:18 +0530

    Add files via upload

---
+++ 6 - Terraform/Terraform Code/1 - Authenticating terraform with Azure/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.14.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+
+  subscription_id = "f721bf30-04fd-4757-a7ad-e1aeeab1a6dc"
+  tenant_id = "b94db9d6-e2d9-4485-ba28-bd37e7a8d30c"
+  client_id = "520c5958-2fd2-45ea-835d-dfcaa1934c0b"
+  client_secret = "~VG8Q~ls_tVcyw1pOua7Pkr.cIaKjXKOs4l3jbGy"
+
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "Learning"
+  location = "West Europe"
+}
